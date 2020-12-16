390e10d18a51d277c5a1542928acacc016988c9f
Daniel Jonathan <daniel@cosmicmind.com>
2020-12-16 15:27:30 -0600

    cleanup of CSS from main composities

--- app/App.vue
+++ app/App.vue
@@ -92,27 +92,6 @@ export default defineComponent({
 <style lang='scss'>
 @import 'tools/_dev';
 
-$composite-search-bar-height-value: 48;
-$composite-search-bar-height: to-px(48);
-
-$composite-drawer-width-value: 260;
-$composite-drawer-width: to-px(260);
-
-$composite-finder-width-value: 400;
-$composite-finder-width: to-px(400);
-
-$composite-menu-width-value: 200;
-$composite-menu-width: to-px(200);
-
-$composite-tools-width-value: 200;
-$composite-tools-width: to-px(200);
-
-$composite-navbar-height-value: 54;
-$composite-navbar-height: to-px(54);
-
-$composite-toolbar-height-value: 48;
-$composite-toolbar-height: to-px(48);
-
 html {
   position: relative;
   width: 100vw;
@@ -125,109 +104,6 @@ body {
   width: 100vw;
   height: 100vh;
   overflow: hidden;
-}
-
-.composite-search-bar {
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  height: $composite-search-bar-height;
-  background-color: orange;
-  z-index: 1;
-}
-
-.composite-drawer {
-  @include margin-block($composite-search-bar-height, 0);
-  
-  position: fixed;
-  top: 0;
-  left: 0;
-  width: $composite-drawer-width;
-  height: 100vh;
-  background-color: purple;
-  z-index: 1;
-}
-
-.composite-finder {
-  @include margin-block($composite-search-bar-height, 0);
-
-  position: fixed;
-  top: 0;
-  right: 0;
-  width: $composite-finder-width;
-  height: 100vh;
-  background-color: blue;
-  z-index: 1;
-}
-
-.composite-navbar {
-  @include margin-block($composite-search-bar-height, 0);
-  @include margin-inline(to-px($composite-drawer-width-value), $composite-finder-width);
-  
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  height: $composite-navbar-height;
-  background-color: yellow;
-  z-index: 1;
-}
-
-.composite-toolbar {
-  @include margin-block(to-px($composite-search-bar-height-value + $composite-navbar-height-value), 0);
-  @include margin-inline(to-px($composite-drawer-width-value), $composite-finder-width);
-  
-  position: fixed;
-  top: 0;
-  left: 0;
-  right: 0;
-  height: $composite-toolbar-height;
-  background-color: magenta;
-  z-index: 1;
-}
-
-.composite-menu {
-  @include margin-block(to-px($composite-search-bar-height-value + $composite-navbar-height-value + $composite-toolbar-height-value), 0);
-  @include margin-inline(to-px($composite-drawer-width-value), 0);
-  
-  position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  width: $composite-menu-width;
-  background-color: cyan;
-  z-index: 0;
-  overflow: auto;
-}
-
-.composite-tools {
-  @include margin-block(to-px($composite-search-bar-height-value + $composite-navbar-height-value + $composite-toolbar-height-value), 0);
-  @include margin-inline(0, $composite-finder-width);
-  
-  position: fixed;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  width: $composite-tools-width;
-  background-color: lightcoral;
-  z-index: 0;
-  overflow: auto;
-}
-
-.composite-main {
-  @include margin-block(to-px($composite-search-bar-height-value + $composite-navbar-height-value + $composite-toolbar-height-value), 0);
-  @include margin-inline(to-px($composite-drawer-width-value + $composite-menu-width-value), 
-                         to-px($composite-finder-width-value + $composite-tools-width-value));
-  
-  position: fixed;
-  top: 0;
-  left: 0;
-  bottom: 0;
-  right: 0;
-  background-color: green;
-  z-index: 0;
-  overflow: auto;
 }
 
 @include is-gte-tablet() {

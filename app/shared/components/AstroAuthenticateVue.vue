017804ae56f26d1e87d8f282d3a14b8b78bcab95
Daniel Jonathan <daniel@cosmicmind.com>
2021-01-06 17:59:57 -0600

    wip: adding astro authenticate vue

---
+++ app/shared/components/AstroAuthenticateVue.vue
@@ -0,0 +1,33 @@
+<!-- Copyright © 2020, CosmicMind, Inc. <http://cosmicmind.com>. All rights reserved. -->
+
+<template lang='pug'>
+aside.astro-authenticate-vue
+    
+</template>
+
+<script lang='ts'>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'CompositeModalVue',
+})
+
+</script>
+
+<style lang='scss'>
+@import 'tools/_dev';
+
+.astro-authenticate-vue {
+  position: absolute;
+
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
+
+  @include theme() {
+    background-color: theme-style(primary-background);
+  }
+}
+
+</style>
